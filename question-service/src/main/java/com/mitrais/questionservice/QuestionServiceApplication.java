915e17d4100e6a428da50dee0c48d3c67e263c80
Rai S. Arijasa <raisuardhyana.arijasa@mitrais.com>
2019-08-05 16:49:38 +0800

    initial project

---
+++ question-service/src/main/java/com/mitrais/questionservice/QuestionServiceApplication.java
@@ -0,0 +1,15 @@
+package com.mitrais.questionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class QuestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionServiceApplication.class, args);
+	}
+
+}
